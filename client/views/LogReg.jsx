c7b4ef79efc02100b0bc76fcccda9e088bf0c538
Ahnaf Khan <ahnafkhan3@gmail.com>
2023-08-03 15:31:34 -0700

    created logout component

--- client/views/LogReg.jsx
+++ client/views/LogReg.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
-const LogReg = ({creator, setCreator}) => {
+const LogReg = ({creator, setCreator, setIsAuthenticated }) => {
   return (
     <>
       <Navbar />
       <main className="bg-dark-subtle">
         {/* <div className="album py-5 bg-dark" style={{ minHeight: '100vh' }}> */}
         <div className="d-flex flex-column align-items-center">
-          <Login />
+          <Login setIsAuthenticated={setIsAuthenticated} />
           <br></br>
           <Register/>
         </div>
