a6d84ae12d0825f276b83ef9244baa01ea2fc9f6
Sam Heck <heck.sam@gmail.com>
2023-08-03 17:53:30 -0700

    remove unused imports on Feed component

--- client/containers/Feed.jsx
+++ client/containers/Feed.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import '../styles.scss';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import Watchlist from '../components/WatchList';
 import Genre from '../components/Genre';
 
 const Feed = ({ getFeed, videoList, setVideoList }) => {
